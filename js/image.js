1b7752c76e4a729748fae954f7fa3a225fb6c2c8
algilardi <algilardi95@gmail.com>
2017-03-28 15:05:10 -0400

    image modal done

--- js/image.js
+++ js/image.js
@@ -1,14 +1,34 @@
+// Image modal code from: https://www.w3schools.com/howto/howto_css_modal_images.asp
+
 // Get the modal
 var modal = document.getElementById('img-modal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('img1');
-var modalImg = document.getElementById("modal-img1");
-img.onclick = function(){
-    console.log("clicked");
+var img1 = document.getElementById('ball-img');
+var img2 = document.getElementById('coat-img');
+var img3 = document.getElementById('mtg-img');
+var img4 = document.getElementById('catan-img');
+
+var modalImg = document.getElementById("modal-img");
+
+img1.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
+}
+
+img2.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+}
+
+img3.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+}
+
+img4.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
 }
 
 // Get the <span> element that closes the modal
